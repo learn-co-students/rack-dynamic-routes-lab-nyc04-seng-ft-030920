5d15958dce70e6c006dd5d7bac56ae46e1255fa5
replacem3nts <donny.landis@gmail.com>
2020-03-29 14:28:44 -0400

    refactors to separate Item class functions

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
 
         if req.path.match(/items/)
             item_name = req.path.split("/items/").last
-            item_instance = all_items.detect {|item| item.name == item_name}
+            item_instance = detect_item_by_name(item_name)
             if item_instance
-                resp.write "#{item_instance.price}"
+                resp.write "#{get_item_price(item_instance)}"
             else
                 resp.write "Item not found"
                 resp.status = 400
@@ -17,6 +17,14 @@ class Application
             resp.status = 404
         end
         resp.finish
+    end
+
+    def get_item_price(item)
+        item.price
+    end
+
+    def detect_item_by_name(item_name)
+        all_items.detect {|item| item.name == item_name}
     end
 
     def all_items
